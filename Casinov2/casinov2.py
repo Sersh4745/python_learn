dee86bde17dd17a70840b6c6d3f8993e952dd36b
Sersh4745 <Sersh4745@gmail.com>
2020-12-23 23:34:30 +0200

    v3

--- Casinov2/casinov2.py
+++ Casinov2/casinov2.py
@@ -1,6 +1,23 @@
 import json
-from os import name
 import random
+
+
+STATISTIC_TEMPLATE = """
+Имя игрока: {name}
+Количество очков: {points}
+
+Magic:
+    Всего игр сыграно: {magic_games}
+    Выиграно: {magic_wins}
+    Коэфициент выграшей: {mg_kof}
+    Рекордное количество попыток: {mg_rec}
+
+BlackJack:
+    Всего игр сыграно: {bj_games}
+    Выиграно: {bj_wins}
+    Коэфициент выграшей: {bj_kof}
+"""
+
 
 class Casino:
     player_name = None
@@ -103,7 +120,7 @@ class Casino:
                 number = int(input('Введите число от 1 до 100: \n'))
                 counter += 1
             except ValueError:
-                continue        
+                continue
 
             if counter >= 25:  # дано 25 попыток угадать число
                 print('Вы проиграли: ')
@@ -120,7 +137,7 @@ class Casino:
                 print('Твое число меньше, попытайся еще')
             else:
                 print('\nТы угадал, это число ', number)
-                if self.player_data['magic']['record'] == None:
+                if self.player_data['magic']['record'] is None:
                     print('Супер, Это рекорд! Кол попыток: ', counter)
                     self.player_data['magic']['record'] = counter
                 elif counter < self.player_data['magic']['record']:
@@ -139,12 +156,6 @@ class Casino:
                     return self.main_menu()
                 random_number = random.randint(1, 100)
                 counter = 0
-            # self.player_data['points'] = point
-            # self.player_data['magic']['games'] = counter_pusk
-            # self.player_data['magic']['wins'] = pusk_plus
-            # self.player_data['magic']['record'] = record_counter
-            # self.player_data['magic']['koef'] = payoff_ratio
-      
 
     def blackjack(self):
         """
@@ -162,16 +173,12 @@ class Casino:
         ] * 4 * random.randint(1, 8)
         # от 1 до 8 колод
         count = 0
-        # self.player_data['blackjack']['games'] = count_play
-        # self.player_data['blackjack']['wins'] = play_win
-        # self.player_data['blackjack']['koef'] = payoff_rat
-
         random.shuffle(koloda)
         print('''Игра Blackjack
         Правила:
         Участник берет карту до тех пор - пока не достигнет в сумме 21
         Если сумма карт выше 21 - то Вы проиграли''')
-        
+
         try:
             rate = int(input('\nСделайте ставку: '))  # ставка
             self.player_data['blackjack']['games'] += 1
@@ -187,7 +194,7 @@ class Casino:
                 print('Вам попалась карта достоинством', current)
                 count += current
                 if count > 21:
-                    self.player_data['points'] -= rate  # отнимаем ставку в случе проиграша
+                    self.player_data['points'] -= rate
                     print('Увы перебор')
                     return self.main_menu()
                 elif count == 21:
@@ -202,7 +209,7 @@ class Casino:
             elif choice != 'y':
                 print('У вас', count, 'очков и вы закончили игру.')
                 return self.main_menu()
-        
+
     def statistic(self):
         """
             TODO: Здесь будет формироваться и выводится статистика.
@@ -210,27 +217,28 @@ class Casino:
             Просчитываются коэфициэнты,
             составляется нужное сообщение с помощью форматирования строк.
         """
-        print('\nИмя игрока:', self.player_name)
-        print('Количество очков:', self.player_data['points'])
-        print('\nMagic')
-        print('Всего игр сыграно: ', self.player_data['magic']['games'])
-        print('Выиграно: ', self.player_data['magic']['wins'])
-        if self.player_data['magic']['games'] != 0:
-            print('Коэфициент выграшей: ', self.player_data['magic']['wins']/self.player_data['magic']['games'])
+        s = self.player_name
+        p = self.player_data['points']
+        g = self.player_data['magic']['games']
+        w = self.player_data['magic']['wins']
+        r = self.player_data['magic']['record']
+        bw = self.player_data['blackjack']['wins']
+        br = self.player_data['blackjack']['games']
+        mg_k = w/g if g != 0 else 0.0
+        r = r if r is not None else 0
+        bj_k = bw/br if br != 0 else 0.0
+
+        statistic = STATISTIC_TEMPLATE.format(
+            name=s, points=p, magic_games=g, magic_wins=w, mg_rec=r,
+            mg_kof=mg_k, bj_games=br, bj_wins=bw, bj_kof=bj_k
+        )
+        print(statistic)
+        choice_ = input('\nВывести главное меню? (y/n) ')
+
+        if choice_ == 'y':
+            return self.main_menu()
         else:
-            print('Коэфициент выграшей: ', 0.0)
-        if self.player_data['magic']['record'] != None:
-            print('Рекордное количество попыток: ', self.player_data['magic']['record'])
-        else:
-            print('Рекордное количество попыток: ', 0)
-        print('\nBlackjack')
-        print('Всего игр сыграно: ', self.player_data['blackjack']['games'])
-        print('Выиграно: ', self.player_data['blackjack']['wins'])
-        if self.player_data['blackjack']['games'] != 0:
-            print('Коэфициент выграшей: ', self.player_data['blackjack']['wins']/self.player_data['blackjack']['games'])
-        else:
-            print('Коэфициент выграшей: ', 0.0)
-        
+            print('\nДо встречи !')
 
     def reset(self):
         """
@@ -241,14 +249,15 @@ class Casino:
 
             Либо сделать выбор: обнулить статистику или удалить игрока.
         """
-        print('\nУдалить прогрес игрока', self.player_name,'?')
+        print('\nУдалить прогрес игрока', self.player_name, '?')
         if input('y or n: ') == 'y':
             del self.player_data
+            self.__del__()
         return self.launch()
 
     def __del__(self):
         """ При удалении экземпляра обновляем информацию в файле """
-        with open('casino.json', 'r') as f:
+        with open('casino.json', 'r+') as f:
             data = json.load(f)
         data[self.player_name] = self.player_data
         json_data = json.dumps(data, indent=4)
